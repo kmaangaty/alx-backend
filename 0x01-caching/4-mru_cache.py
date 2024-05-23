e053bc2dc0ec34b4333b307f2a539c3577272aff
Kmangaty <kmaangaty@gmail.com>
2024-05-23 22:26:17 +0300

    setup

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,25 +1,45 @@
-#!/usr/bin/python3
-""" 4-mru_cache """
+#!/usr/bin/env python3
+"""Task 4: MRU Caching.
+"""
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ Defines a MRU caching system """
+    """A class MRUCache that inherits from BaseCaching and implements
+    a caching system based on the Most Recently Used (MRU) algorithm.
+    """
+
+    def __init__(self):
+        """Initializes the MRU cache."""
+        super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Adds an item to the cache """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                del self.cache_data[key]
-            elif len(self.cache_data) >= self.MAX_ITEMS:
-                most_recent_key = max(self.cache_data, key=self.cache_data.get)
-                del self.cache_data[most_recent_key]
-                print("DISCARD:", most_recent_key)
+        """Adds an item to the cache."""
+        if key is None or item is None:
+            return
+
+        # Check if key already exists
+        if key in self.cache_data:
+            # Update item and move it to front
             self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            # Add new item
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                # Remove the most recently used item
+                mru_key, _ = self.cache_data.popitem(last=False)
+                print("DISCARD:", mru_key)
+            # Add the new item and mark it as most recently used
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
 
     def get(self, key):
-        """ Retrieves an item from the cache """
-        if key is None or key not in self.cache_data:
-            return None
-        self.cache_data.move_to_end(key)
-        return self.cache_data[key]
+        """Retrieves an item from the cache by key."""
+        if key is not None and key in self.cache_data:
+            # Move the item to front (most recently used)
+            self.cache_data.move_to_end(key, last=False)
+        # Return the item if found, otherwise None
+        return self.cache_data.get(key, None)
