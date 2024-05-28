0c374913494de794645b701f72b7d59880c47b61
Your Name <you@example.com>
2024-05-28 11:49:41 +0000

    132

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with Babel setup
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """Configuration for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Route to render index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with locale detection
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """Configuration for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determine the best match with supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Route to render index.html"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
